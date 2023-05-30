3a5f43f58537fd7a7f5e02f4e4fe1556fb9e5b36
NaassomDourado <naassomdourado@gmail.com>
2023-05-30 17:44:21 -0300

    Blissify

--- bliss_berlin.mk
+++ bliss_berlin.mk
@@ -22,13 +22,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/berlin/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_berlin
+PRODUCT_NAME := bliss_berlin
 PRODUCT_DEVICE := berlin
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_BRAND := motorola
-PRODUCT_MODEL := motorola edge 20
+PRODUCT_MODEL := edge 20
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
